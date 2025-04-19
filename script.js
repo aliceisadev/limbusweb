7dfc20235d229c8ecdf76a5fd57e79bc2cd836c5
Alice <malkenjon36@gmail.com>
2025-04-19 14:35:29 -0400

    finished adding images
    also changed some js

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ document.addEventListener('keydown', function(event) {
         console.log('Enter key was pressed!');
     }
 });
-$(function() {
-    $('.icon2').hover(function() {
-    $('#b').css('background-color', 'yellow');
-    }, function() {
-    // on mouseout, reset the background colour
-    $('#b').css('background-color', '');
-});
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const icon = document.querySelector('.icon1');
+    const yi = document.querySelector('.yi');
+
+    icon.addEventListener('mouseenter', () => {
+      yi.classList.add('hovered');
+    });
+
+    icon.addEventListener('mouseleave', () => {
+      yi.classList.remove('hovered');
+    });
+  });
